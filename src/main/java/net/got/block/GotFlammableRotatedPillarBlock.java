21f932453f77470d6e7ad3e2fd71d6aa86194d0d
Frank123514 <eternalgoonr@gmail.com>
2026-06-01 02:49:40 -0500

    stripping fixes

--- src/main/java/net/got/block/GotFlammableRotatedPillarBlock.java
+++ src/main/java/net/got/block/GotFlammableRotatedPillarBlock.java
@@ -46,16 +46,20 @@ public class GotFlammableRotatedPillarBlock extends RotatedPillarBlock {
             // Check logs
             for (String woodType : GotModBlocks.LOGS.keySet()) {
                 if (GotModBlocks.LOGS.get(woodType).get() == currentBlock) {
-                    Block strippedLog = GotModBlocks.STRIPPED_LOGS.get(woodType).get();
-                    return strippedLog.defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+                    var strippedLogDeferred = GotModBlocks.STRIPPED_LOGS.get(woodType);
+                    if (strippedLogDeferred != null) {
+                        return strippedLogDeferred.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+                    }
                 }
             }
 
             // Check woods (bark-all-sides)
             for (String woodType : GotModBlocks.WOODS.keySet()) {
                 if (GotModBlocks.WOODS.get(woodType).get() == currentBlock) {
-                    Block strippedWood = GotModBlocks.STRIPPED_WOODS.get(woodType).get();
-                    return strippedWood.defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+                    var strippedWoodDeferred = GotModBlocks.STRIPPED_WOODS.get(woodType);
+                    if (strippedWoodDeferred != null) {
+                        return strippedWoodDeferred.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+                    }
                 }
             }
         }
