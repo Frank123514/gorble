c8fa935123bb1be5adbd1eb0393c8e4f14c197d4
Frank123514 <eternalgoonr@gmail.com>
2026-06-08 20:34:28 -0500

    updates

--- src/main/java/net/got/block/GotShortGrassBlock.java
+++ src/main/java/net/got/block/GotShortGrassBlock.java
@@ -1,11 +1,18 @@
 package net.got.block;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GotShortGrassBlock extends BushBlock {
 
     public static final MapCodec<GotShortGrassBlock> CODEC = MapCodec.unit(() -> null);
+
+    protected static final VoxelShape SHAPE = box(5.0, 0.0, 5.0, 11.0, 10.0, 11.0);
 
     public GotShortGrassBlock(Properties properties) {
         super(properties);
@@ -14,5 +21,10 @@ public class GotShortGrassBlock extends BushBlock {
     @Override
     protected MapCodec<? extends BushBlock> codec() {
         return CODEC;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 }
